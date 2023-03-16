be2489f264b2117e4ff0017b3f4a84c80bfd28d3
alinurammar <aalinur@stanford.edu>
2023-03-16 11:01:46 -0700

    nit

--- poller/evm/client/evm_client/config.go
+++ poller/evm/client/evm_client/config.go
@@ -1,8 +1,6 @@
 package evm_client
 
 import (
-	"time"
-
 	"github.com/spf13/viper"
 
 	"github.com/coherent-api/contract-poller/shared/constants"
@@ -11,12 +9,9 @@ import (
 type Config struct {
 	Blockchain constants.Blockchain
 
-	EthNodeRPC                string
-	PolyNodeRPC               string
-	OptNodeRPC                string
-	EnrichTransactionsTimeout time.Duration
-
-	FetchBlockTimeout time.Duration
+	EthNodeRPC  string
+	PolyNodeRPC string
+	OptNodeRPC  string
 }
 
 func NewConfig() *Config {
@@ -24,20 +19,15 @@ func NewConfig() *Config {
 
 	viper.AutomaticEnv()
 	config := Config{
-		Blockchain:                constants.Blockchain(viper.GetString("blockchain")),
-		EthNodeRPC:                viper.GetString("ethereum_node_rpc_endpoint"),
-		OptNodeRPC:                viper.GetString("optimism_node_rpc_endpoint"),
-		PolyNodeRPC:               viper.GetString("polygon_node_rpc_endpoint"),
-		EnrichTransactionsTimeout: viper.GetDuration("enrich_transactions_timeout"),
-		FetchBlockTimeout:         viper.GetDuration("fetch_block_timeout"),
+		Blockchain:  constants.Blockchain(viper.GetString("blockchain")),
+		EthNodeRPC:  viper.GetString("ethereum_node_rpc_endpoint"),
+		OptNodeRPC:  viper.GetString("optimism_node_rpc_endpoint"),
+		PolyNodeRPC: viper.GetString("polygon_node_rpc_endpoint"),
 	}
 
 	return &config
 }
 
 func setDefaults() {
-	viper.SetDefault("rpc_retries", 2)
-	viper.SetDefault("rpc_timeout", "20000ms")
-	viper.SetDefault("fetch_block_timeout", "14s")
 	viper.SetDefault("blockchain", "ethereum")
 }
